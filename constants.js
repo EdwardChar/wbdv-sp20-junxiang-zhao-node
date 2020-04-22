6aa620be0c18ed17fcef5bcd053419bacb57d43c
edwardchar <edwardchar@yahoo.com>
2020-04-21 21:49:13 -0400

    A9

---
+++ constants.js
@@ -0,0 +1,2 @@
+const MONGODB_URL = 'mongodb://localhost:27017/A9-local';
+module.exports = MONGODB_URL;
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const MONGODB_URL = require('./constants');
+
+var connectionString = MONGODB_URL;
+if(process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds251877.mlab.com:51877/heroku_h41wpr5l';
+}
+
+
+const mongoose = require('mongoose')
+mongoose.connect(connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true })
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
@@ -10,8 +27,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-require('./controllers/quiz.controller.server')(app);
-require('./controllers/question.controller.server')(app);
+require('./controllers/quizzes.controller.server')(app)
+require('./controllers/questions.controller.server')(app)
+require('./controllers/quiz-attempts.controller.server')(app)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
